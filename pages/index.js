485df55667a93a48f62dab3f153d8ccc067a781c
yoojat <yooja.ty@gmail.com>
2021-03-21 14:29:28 +0900

    seogyedong coffee init

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,116 @@
+import 'react-slideshow-image/dist/styles.css'
+import styled from 'styled-components'
+import { Slide } from 'react-slideshow-image';
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+
+
+const slideImages = [
+  '/images/1.jpeg',
+  '/images/2.jpeg',
+  '/images/3.jpeg',
+  '/images/4.jpeg',
+  '/images/5.jpeg',
+  '/images/6.jpeg',
+  '/images/7.jpeg',
+];
+
+const Top = styled.div`
+  text-align : center;
+  font-size:30px;
+  margin-top: 30px;
+  margin-bottom: 30px;
+`;
+
+const LogoImg = styled.img`
+width: 200px;
+`;
+
+const Card = styled.div`
+height : 400px;
+max-width: 400px;
+background-size: contain;
+background-repeat : no-repeat;
+background-position: center;
+margin-left : auto;
+margin-right : auto;
+`;
+const SliderContainer = styled.div`
+margin-left: 30px;
+margin-right: 30px;
+`;
+
+const CardContainer = styled.div`
+`;
+
+const MenuContainer = styled.div`
+text-align: center;
+margin-top: 5px;
+display: flex;
+justify-content: center;
+`;
+
+const Introduce = styled.div`
+text-align : center;
+font-size: 14px;
+margin-top: 13px;
+margin-bottom: 20px;
+`;
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+    <>
+    <Head>
+        <title>서계동 커피</title>
+        <link rel="icon" href="/cropped-baraboda-favicon-32x32.png" sizes="32x32"></link>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge"></meta>
+        <meta property="og:locale" content="ko_KR"></meta>
+        <meta property="og:site_name" content="서계동 커피"></meta>
+        <meta property="og:type" content="website"></meta>
+        <meta charset="utf-8"></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="시같은 공간. 만리재로156 3층. OPEN 월화수목일 11:30-18:00. 금토 11:30-20:00"/>
+        <meta name="keywords" content="서계동 커피"/>
+        <meta property="og:site_name" content="서계동 커피"/>
+        <meta property="og:title" content="서계동 커피"></meta>
+        <meta property="og:description" content="시같은 공간. 만리재로156 3층. OPEN 월화수목일 11:30-18:00. 금토 11:30-20:00"/>
+        <meta name="twitter:title" content="서계동 커피"></meta>
+        <meta name="twitter:description" content="시같은 공간. 만리재로156 3층. OPEN 월화수목일 11:30-18:00. 금토 11:30-20:00"></meta>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+    </Head>
+    <Top>
+      <LogoImg src={"/logo.png"}/>
+    </Top>
+      <SliderContainer>
+      <Slide arrows={false}>
+          <CardContainer className="each-slide">
+            <Card style={{'backgroundImage': `url(${slideImages[0]})`}}>
+            </Card>
+          </CardContainer>
+          <CardContainer className="each-slide">
+            <Card style={{'backgroundImage': `url(${slideImages[1]})`}}>
+            </Card>
+          </CardContainer>
+          <CardContainer className="each-slide">
+            <Card style={{'backgroundImage': `url(${slideImages[2]})`}}>
+            </Card>
+          </CardContainer>
+        </Slide>
+      </SliderContainer>
+      <Introduce>
+        <div>
+         시같은 공간.
         </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+        <div>
+          만리재로156 3층. OPEN
+        </div>
+        <div>
+          월화수목일 11:30-18:00
+        </div>
+        <div>
+          금토 11:30-20:00
+        </div>
+      </Introduce>
+    </>
   )
 }
